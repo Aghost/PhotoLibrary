2ec1649d28ba753f39ee41101f9a94f8a78d1951
Aghost <youri_van_sorgen2@example.com>
2021-10-05 11:58:22 +0200

    close button does not work

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { Component, NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { PhotoDetailComponent } from './photo-detail/photo-detail.component';
+import { PhotoGridComponent } from './photo-grid/photo-grid.component';
+
+const routes: Routes = [
+  {path: "" ,component: PhotoGridComponent},
+  {path:"photo", component:PhotoDetailComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
